kylemart/process-scheduler

commit 132cb76ced476786d536d283bce38e40ecdc9b60

Refactored RR logic

src/scheduler.c
@@ -142,33 +142,28 @@ void run_rr(FILE *out, uint runfor, uint quantum, ProcessList *processes)
     for (uint tick = 0; tick < runfor; ++tick) {
         for (size_t i = 0; i < jobcount; ++i) {
             Job *job = &jobs[i];
-            if (job->start > tick || job->burst == 0) {
-                continue;
+            if (job->start < tick && job->burst != 0) {
+                if (job != selected) {
+                    ++job->wait;
+                }
+                ++job->turnaround;
             }
-            if (job->start == tick) {
+            else if (job->start == tick) {
                 jobq_add(ready, job);
                 fprintf(out, "Time %u: %s arrived\n", tick, job->name);
-                continue;
             }
-            if (job != selected) {
-                ++job->wait;
-            }
-            ++job->turnaround;
         }
 
         if (selected) {
             --timeleft;
             --selected->burst;
-            if (timeleft > 0) {
-                continue;
-            }
-            if (selected->burst > 0) {
-                jobq_lshift(ready);
-            }
-            else {
+            if (selected->burst == 0) {
                 Job *done = jobq_remove(ready);
                 fprintf(out, "Time %u: %s finished\n", tick, done->name);
                 selected = NULL;
+            }
+            else if (timeleft == 0) {
+                jobq_lshift(ready);
             }
         }
 

commit 758fc1da69a06fee0581a394c194c7eca8a62c68

Fixed bugs introduced in 132cb76

src/scheduler.c
@@ -164,13 +164,14 @@ void run_rr(FILE *out, uint runfor, uint quantum, ProcessList *processes)
             }
             else if (timeleft == 0) {
                 jobq_lshift(ready);
+                selected = NULL;
             }
         }
 
         if (jobq_empty(ready)) {
             fprintf(out, "Time %u: IDLE\n", tick);
         }
-        else {
+        else if (!selected) {
             selected = jobq_peek(ready);
             timeleft = min(selected->burst, quantum);
             fprintf(out, "Time %u: %s selected (burst %u)\n", tick,