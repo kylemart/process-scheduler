c44ab274021f29835ee5072dcfe1cf0e767d288b
Kyle Martinez <git@kylem.art>
2018-02-16 11:49:22 -0500

    Added test for when processes in fcfs are unordered

--- tests/acceptance.py
+++ tests/acceptance.py
@@ -8,7 +8,7 @@ from shutil import copy
 import os
 import sys
 
-NUM_TESTCASES = 4
+NUM_TESTCASES = 5
 
 print("======================================================================")
 print("COMPILING")
